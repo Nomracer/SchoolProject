204d805dc5b315774051158312389866a821d1f9
Gürkan <gurkansimsek2153@gmail.com>
2022-12-21 14:00:17 +0300

    masterofpuppets

--- rent.js
+++ rent.js
@@ -4,4 +4,13 @@ const mongose=require();
 const bodyParser=require();
 app.use(bodyParser.urlencoded((extended=>true)));
 
-mongoose.connect("mongob")
+mongoose.connect("mongodb+srv://Nomracer:<password>@cluster0.ms8gvc5.mongodb.net/?retryWrites=true&w=majority",
+{ useNewUrlParser:true,
+    useUnifiedTopology:true,  
+}).then(console.log('connect to mongo db!'))
+
+app.listen(3000,function(){
+    console.log("server is running on http://localhost:3000")
+
+
+})
