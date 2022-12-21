1bf0b82d0322b50df8c11678c3f0c66e7a94246b
Gürkan <gurkansimsek2153@gmail.com>
2022-12-21 14:22:30 +0300

    masterofpuppets

--- rent.js
+++ rent.js
@@ -1,16 +1,21 @@
-const express=require("express");
-const app=express();
-const mongose=require();
-const bodyParser=require();
-app.use(bodyParser.urlencoded((extended=>true)));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-mongoose.connect("mongodb+srv://Nomracer:<password>@cluster0.ms8gvc5.mongodb.net/?retryWrites=true&w=majority",
-{ useNewUrlParser:true,
-    useUnifiedTopology:true,  
-}).then(console.log('connect to mongo db!'))
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBdxWH1ur8psaS6LQw9dwjOcwpY1Rv8ZY8",
+  authDomain: "rentingcar-8d0c1.firebaseapp.com",
+  projectId: "rentingcar-8d0c1",
+  storageBucket: "rentingcar-8d0c1.appspot.com",
+  messagingSenderId: "111974221711",
+  appId: "1:111974221711:web:c720167b7a2ba49ceb6dfa",
+  measurementId: "G-FJ0EE4G9VT"
+};
 
-app.listen(3000,function(){
-    console.log("server is running on http://localhost:3000 ")
-
-
-})
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
