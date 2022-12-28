44f1425ba29f6ddb4de52437ed9a0e4f6ba0e60e
Gürkan <gurkansimsek2153@gmail.com>
2022-12-28 20:35:39 +0300

    masterofpuppets

--- rent.js
+++ rent.js
@@ -1,11 +1,14 @@
-// Import the functions you need from the SDKs you need
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBdxWH1ur8psaS6LQw9dwjOcwpY1Rv8ZY8",
   authDomain: "rentingcar-8d0c1.firebaseapp.com",
@@ -16,6 +19,6 @@ const firebaseConfig = {
   measurementId: "G-FJ0EE4G9VT"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
