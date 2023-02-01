53f83b4c4e3d48ec57ef23b27e79454e7c4d5637
Gürkan <gurkansimsek2153@gmail.com>
2023-02-01 15:24:06 +0300

    masterofpuppets

---
+++ js/general.js
@@ -0,0 +1,21 @@
+
+
+	const firebaseConfig = {
+	  apiKey: "AIzaSyCdOusBl6foONaWOpDkGJLViY3J2wNPcaE",
+	  authDomain: "nomrent-fc5e7.firebaseapp.com",
+	  projectId: "nomrent-fc5e7",
+	  storageBucket: "nomrent-fc5e7.appspot.com",
+	  messagingSenderId: "207305368484",
+	  appId: "1:207305368484:web:604cd9e46581468bdf5731"
+	};
+  
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+    const auth = firebase.auth()
+    const database = firebase.database()
+
+    // register function
+    function register(){
+
+        
+    }
