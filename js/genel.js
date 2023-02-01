79b02ed21806c21f8d6efc28b132c4f17aad5106
Gürkan <gurkansimsek2153@gmail.com>
2023-02-01 20:39:23 +0300

    masterofpuppets

---
+++ js/genel.js
@@ -0,0 +1,77 @@
+
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+
+	const firebaseConfig = {
+	  apiKey: "AIzaSyCdOusBl6foONaWOpDkGJLViY3J2wNPcaE",
+	  authDomain: "nomrent-fc5e7.firebaseapp.com",
+	  projectId: "nomrent-fc5e7",
+	  storageBucket: "nomrent-fc5e7.appspot.com",
+	  messagingSenderId: "207305368484",
+	  appId: "1:207305368484:web:604cd9e46581468bdf5731"
+	};
+  
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+    const auth = firebase.auth()
+    const database = firebase.database()
+
+    // register function
+    function register(){
+    }
+
+	let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
