2bf0d14738a5b5b1d08ca159bc6510f3127bf764
Gürkan <gurkansimsek2153@gmail.com>
2023-02-03 01:33:18 +0300

    masterofpuppets

--- js/genel.js
+++ js/genel.js
@@ -1,5 +1,4 @@
 
-console.clear();
 
 const loginBtn = document.getElementById('login');
 const signupBtn = document.getElementById('signup');
@@ -28,50 +27,3 @@ signupBtn.addEventListener('click', (e) => {
 	});
 });
 
-
-	const firebaseConfig = {
-	  apiKey: "AIzaSyCdOusBl6foONaWOpDkGJLViY3J2wNPcaE",
-	  authDomain: "nomrent-fc5e7.firebaseapp.com",
-	  projectId: "nomrent-fc5e7",
-	  storageBucket: "nomrent-fc5e7.appspot.com",
-	  messagingSenderId: "207305368484",
-	  appId: "1:207305368484:web:604cd9e46581468bdf5731"
-	};
-  
-	// Initialize Firebase
-	const app = initializeApp(firebaseConfig);
-    const auth = firebase.auth()
-    const database = firebase.database()
-
-    // register function
-    function register(){
-    }
-
-	let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
